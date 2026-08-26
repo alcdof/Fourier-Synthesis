4022180b62294c87f9778a4ae3b7ba984bcbf2b1
André Caillot <andreluizfilho@alunos.utfpr.edu.br>
2026-08-26 13:49:03 -0300

    implemented some of the necessary functions

--- fourier_synthesis.py
+++ fourier_synthesis.py
@@ -4,6 +4,7 @@
 
 # fourier_synthesis.py
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Parâmetros estruturais (Não alterar)
 T0 = 2.0
@@ -11,21 +12,14 @@ t = np.arange(0, 2.001, 0.001)
 N = 19
 
 def calcular_frequencia_fundamental(T0):
-    """
-    Retorna a frequência fundamental w0 em rad/s.
-    """
-    # TODO: Calcule e retorne w0 = 2 * pi / T0
-    w0 = None
+    w0 = 2 * np.pi / T0
+
     return w0
 
 def gerar_sinal_ideal(t):
-    """
-    Gera o sinal quadrado ideal g(t) baseado no vetor de tempo t.
-    Deve ser 1.0 nos intervalos [0.0, 0.5[ e [1.5, 2.0], e -1.0 no intervalo [0.5, 1.5[.
-    """
-    # TODO: Implemente os intervalos do sinal ideal quadrado
-    g_ideal = None
-    return g_ideal
+    condicao = ((t >= 0.0) & (t < 0.5) | (t >= 1.5) & (t <= 2.0))
+        
+    return np.where(condicao, 1.0, -1.0)
 
 def aproximar_fourier(t, w0, N):
     """
@@ -47,10 +41,27 @@ def calcular_mse(g_ideal, g_approx):
     mse = None
     return mse
 
+def plot_g_ideal(t):
+    g = gerar_sinal_ideal(t)
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(t, g, label='g(t) ideal', color='blue', linewidth=2)
+    plt.title('Sinal Quadrado Ideal')
+    plt.xlabel('Tempo (t)')
+    plt.ylabel('Amplitude')
+    plt.ylim(-1.5, 1.5)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
 # Se o script for executado diretamente, mostra os resultados no console
 if __name__ == "__main__":
     w0 = calcular_frequencia_fundamental(T0)
     g_ideal = gerar_sinal_ideal(t)
+
+    t_vetor = np.linspace(0, 2, 2001)
+    plot_g_ideal(t)
+
     if w0 is not None and g_ideal is not None:
         g_approx, PN = aproximar_fourier(t, w0, N)
         if g_approx is not None and PN is not None:
